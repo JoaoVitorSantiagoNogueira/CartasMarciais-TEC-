2941af26bcff97c125d950641458e8510870a9c2
JoaoVitorSantiagoNogueira <65747315+JoaoVitorSantiagoNogueira@users.noreply.github.com>
2021-12-15 23:46:17 -0300

    adicionando funcionalidades basicas do mouse para mover objetos

---
+++ unity/Cartas Marciais/Assets/Scripts/CardUI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardUI : MonoBehaviour
+{
+    Vector3 selectedMousePosition, basePosition;
+    float cameraDistance;
+    bool selectedToMove = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        basePosition = transform.position;
+        cameraDistance = Camera.main.transform.position.y - basePosition.y;;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (selectedToMove)
+        {
+            Vector3  dif =  Camera.main.ScreenToWorldPoint(Input.mousePosition+cameraDistance*Vector3.forward)-selectedMousePosition ;
+            transform.position = dif + basePosition;
+        }              
+    }
+
+    public void startDrag(Vector3 selectedMousePosition)
+    {
+        selectedToMove = true; 
+        this.selectedMousePosition = Camera.main.ScreenToWorldPoint(selectedMousePosition+cameraDistance*Vector3.forward);
+    }
+}
