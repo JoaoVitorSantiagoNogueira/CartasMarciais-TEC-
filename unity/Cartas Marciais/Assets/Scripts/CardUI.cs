6cda0e17b23b751f6bafbc4435e442162af1925d
JoaoVitorSantiagoNogueira <65747315+JoaoVitorSantiagoNogueira@users.noreply.github.com>
2021-12-26 19:22:28 -0300

    criação de zonas + gitignore

--- unity/Cartas Marciais/Assets/Scripts/CardUI.cs
+++ unity/Cartas Marciais/Assets/Scripts/CardUI.cs
@@ -18,6 +18,8 @@ public class CardUI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonUp(0))
+            releaseDrag(Vector3.zero);
         if (selectedToMove)
         {
             Vector3  dif =  Camera.main.ScreenToWorldPoint(Input.mousePosition+cameraDistance*Vector3.forward)-selectedMousePosition ;
@@ -27,7 +29,23 @@ public class CardUI : MonoBehaviour
 
     public void startDrag(Vector3 selectedMousePosition)
     {
+        if (selectedToMove == false)
+        {
+        basePosition = transform.position;
+        cameraDistance = Camera.main.transform.position.y - basePosition.y;
         selectedToMove = true; 
         this.selectedMousePosition = Camera.main.ScreenToWorldPoint(selectedMousePosition+cameraDistance*Vector3.forward);
+        }
+    }
+
+    public void releaseDrag(Vector3 selectedMousePosition)
+    {
+        // if not in a place where you can change zones
+        if (selectedToMove == true)
+        {
+            selectedToMove = false; 
+            this.selectedMousePosition = Vector3.zero;
+            transform.position = basePosition;
+        }
     }
 }
---
+++ unity/Cartas Marciais/Assets/Scripts/cardZone.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cardZone : MonoBehaviour
+{
+    int cardCap;
+    List<CardUI> cardList;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void AddCard(CardUI cui)
+    {
+        cardList.Add(cui);
+    }
+
+    void AddCard (CardUI cui, int pos)
+    {
+        cardList.Insert(pos, cui);
+    }
+
+    void RemoveCard(int index)
+    {
+        cardList.RemoveAt(index);
+    }
+
+    CardUI GetCard (int index)
+    {
+        return cardList[index];
+    }
+
+    CardUI moveCardFrom (int index)
+    {
+        CardUI c = GetCard(index);
+        RemoveCard(index);
+        return c;
+    }
+    
+}
