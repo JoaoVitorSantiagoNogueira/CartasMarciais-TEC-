551b05dea8fd4f14a762807c2b8c92a03734e373
JoaoVitorSantiagoNogueira <65747315+JoaoVitorSantiagoNogueira@users.noreply.github.com>
2022-02-05 17:56:58 -0300

    adicionado parametros para as cartas

---
+++ unity/Cartas Marciais/Assets/Scripts/cardType.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cardType
+{
+    enum direction
+    {
+        high,
+        mid,
+        low,
+        any
+    }
+
+    enum button
+    {
+        attack,
+        block,
+        move,
+        any
+    }
+    direction d;
+    button b;
+
+
+    public void setInput(string button, string direction)
+    {
+        setButton(button);
+        setDirection(direction);
+    }
+
+    public void setButton(string s)
+    {
+        if (s=="attack" || s=="A"|| s=="a" || s=="Attack")
+            b = button.attack;
+        if (s=="block" || s=="B" || s=="b" || s=="Block")
+            b = button.block;
+        if (s=="move" || s=="M" || s=="m"  || s=="c" || s=="C" || s=="Move")
+            b = button.move;
+        if (s=="any" || s=="*" || s=="Any")
+            b = button.any;
+    }
+
+    public void setDirection(string s)
+    {
+        if (s=="high" || s=="h"|| s=="H" || s=="High")
+            d = direction.high;
+        if (s=="mid" || s=="m" || s=="M" || s=="Mid")
+            d = direction.mid;
+        if (s=="low" || s=="L" || s=="l" || s=="Low")
+            d = direction.low;
+        if (s=="any" || s=="*" || s=="Any")
+            d = direction.any;
+    }
+
+
+    public bool isHigh()
+    {
+        if (d == direction.high || d == direction.any)
+        return true;
+        else
+        return false;
+    }
+
+    public bool isMid()
+    {
+        if (d == direction.mid || d == direction.any)
+        return true;
+        else
+        return false;
+    }
+
+    public bool isLow()
+    {
+        if (d == direction.low || d == direction.any)
+        return true;
+        else
+        return false;
+    }
+
+
+        public bool isAttack()
+    {
+        if (b == button.attack || b == button.any)
+        return true;
+        else
+        return false;
+    }
+
+    public bool isBlock()
+    {
+        if (b == button.block || b == button.block)
+        return true;
+        else
+        return false;
+    }
+
+    public bool isMove()
+    {
+        if (b == button.move || b == button.any)
+        return true;
+        else
+        return false;
+    }
+}
